dbd0f3f0b70d0d26a87851a6dfaeaa1f86a1497d
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2022-01-09 12:35:34 +0100

    Create pokemonRepository.py

---
+++ pokesite/pokemon/src/repository/pokemonRepository.py
@@ -0,0 +1,25 @@
+from pokemon.src.model.PokemonListed import *
+from pokemon.src.model.Pokemon import *
+import requests
+import json
+
+def fetchPokemonListed(url=None) -> PokemonListed:
+    if(url != None):
+        assert isinstance(url, str)
+        result = requests.get(
+            url)
+    else:
+        result = requests.get(
+            'https://pokeapi.co/api/v2/pokemon?limit=10')
+    return pokemon_listed_from_dict(json.loads(result.text))
+
+
+def fetchPokemon(url=None, identifier=None) -> Pokemon:
+    if(url != None):
+        assert isinstance(url, str)
+        result = requests.get(
+            url)
+    else:
+        if(identifier != None):
+            result = requests.get('https://pokeapi.co/api/v2/pokemon/' + identifier)
+    return pokemon_from_dict(json.loads(result.text))
