608baeb1ef190178549a8f9cd3b858788bff462b
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2022-01-09 12:35:51 +0100

    Setting up urls

--- pokesite/pokemon/urls.py
+++ pokesite/pokemon/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('pokemons', views.list_pokemon, name="list_pokemon")
 ]
