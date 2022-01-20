fbd31db88d2d6917f96631c722f1807b2ce75811
Baptiste <baptiste.lecat44@gmail.com>
2022-01-20 18:10:44 +0100

    Model for team of pokemons

--- pokesite/pokemon/models.py
+++ pokesite/pokemon/models.py
@@ -1,3 +1,17 @@
+from os import name
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+class PokemonSelected(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.TextField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
